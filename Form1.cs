345a2d5670f3846ed7eae003b2480304e328d71e
FarukOkumus0 <farukokumus94@gmail.com>
2024-12-26 00:21:34 +0300

    LibraryTest
    a test for exercise

---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Kitaplık_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Administrator\Desktop\kitaplık.mdb");
+
+        void listele()
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From Kitaplar", baglanti);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        string durum = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut1 = new OleDbCommand("insert into Kitaplar (KitapAd,Yazar,Tur,Sayfa,Durum) values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", textBox1.Text);
+            komut1.Parameters.AddWithValue("@p2", textBox2.Text);
+            komut1.Parameters.AddWithValue("@p3", comboBox1.Text);
+            komut1.Parameters.AddWithValue("@p4", textBox3.Text);
+            komut1.Parameters.AddWithValue("@p5", durum);
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Sisteme Kaydedildi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "0";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "1";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            textBox4.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            textBox1.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            if (dataGridView1.Rows[secilen].Cells[5].Value.ToString() == "1")
+            {
+                radioButton2.Checked = true;
+            }
+
+            else
+            {
+                radioButton1.Checked = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("Delete From Kitaplar where Kitapid=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", textBox4.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Listeden Silindi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            listele();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("Update Kitaplar Set KitapAd=@p1, Yazar=@p2, Tur=@p3, Sayfa=@p4, Durum=@p5 where Kitapid=@p6", baglanti);
+            komut.Parameters.AddWithValue("@p1", textBox1.Text);
+            komut.Parameters.AddWithValue("@p2", textBox2.Text);
+            komut.Parameters.AddWithValue("@p3", comboBox1.Text);
+            komut.Parameters.AddWithValue("@p4", textBox3.Text);
+            if (radioButton1.Checked== true)
+            {
+                komut.Parameters.AddWithValue("@p5", durum);
+            }
+            if (radioButton2.Checked== true)
+            {
+                komut.Parameters.AddWithValue("@p5", durum);
+            }
+            komut.Parameters.AddWithValue("@p6", textBox4.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Güncellendi", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            OleDbCommand komut = new OleDbCommand("Select * From Kitaplar where KitapAd=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", textBox5.Text);
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            OleDbCommand komut = new OleDbCommand("Select * From Kitaplar where KitapAd like '%" + textBox5.Text + "%'", baglanti);            
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
